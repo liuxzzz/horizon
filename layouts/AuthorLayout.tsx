733427cea98801e3199e4817c3f5c12ca6426b3a
刘洪壮 <liuhongzhuang@nextmar.com>
2025-06-05 15:07:57 +0800

    更新 README 文件，删除不必要的内容，添加新的博客文章和作者信息，更新标签数据，删除不再使用的文件，调整布局组件，优化代码结构。

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: 刘洪壮 liuhongzhuang@nextmar.com
+ * @Date: 2025-04-06 16:21:48
+ * @LastEditors: 刘洪壮 liuhongzhuang@nextmar.com
+ * @LastEditTime: 2025-06-05 13:54:27
+ * @FilePath: /blog/layouts/AuthorLayout.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { ReactNode } from 'react'
 import type { Authors } from 'contentlayer/generated'
 import SocialIcon from '@/components/social-icons'
@@ -32,7 +40,7 @@ export default function AuthorLayout({ children, content }: Props) {
             )}
             <h3 className="pt-4 pb-2 text-2xl leading-8 font-bold tracking-tight">{name}</h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
-            <div className="text-gray-500 dark:text-gray-400">{company}</div>
+            {/* <div className="text-gray-500 dark:text-gray-400">{company}</div> */}
             <div className="flex space-x-3 pt-6">
               <SocialIcon kind="mail" href={`mailto:${email}`} />
               <SocialIcon kind="github" href={github} />
